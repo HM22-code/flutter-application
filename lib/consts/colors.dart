973258d08b278efb379378925db771c4461a1f7b
HM22-code <H.M-22@outlook.fr>
2025-05-15 12:24:15 +0200

    Update new empty flutter project

--- lib/consts/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bgColor = Color(0xFF1E1E2F);
-const bgDarkColor = Color.fromARGB(255, 15, 15, 26);
-const primaryColor = Color(0xFF3E3E5B);
-const secondaryColor = Color(0xFF4E4E7E);
-const accentColor = Color(0xFF5E5E9E);
-const textColor = Color(0xFFFFFFFF);
-const buttonColor = Color(0xFF007BFF);
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/views/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: "regular",
-        appBarTheme:  AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        )
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
-      home: const Home(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
